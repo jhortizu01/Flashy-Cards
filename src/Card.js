58696747f4879df95fce648fe67a4fee1843aaff
Janika Hortizuela <jhortizuela21@gmail.com>
2021-08-25 14:14:05 -0600

    Run lint on card, corrected warnings

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 class Card {
-    constructor(id, cardQuestion, possibleAnswers, correct) {
-        this.id = id;
-        this.question = cardQuestion;
-        this.answers = possibleAnswers;
-        this.correctAnswer = correct;
-    }
+  constructor(id, cardQuestion, possibleAnswers, correct) {
+    this.id = id;
+    this.question = cardQuestion;
+    this.answers = possibleAnswers;
+    this.correctAnswer = correct;
+  }
 
 }
 
