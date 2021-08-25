af8be111e99c8e24c3d35ca5d5af9e62eef80362
Janika Hortizuela <jhortizuela21@gmail.com>
2021-08-25 15:25:18 -0600

    refactor round-test

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,140 +7,73 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round')
 
 describe('Round', () => {
+
+  let card1, card2, card3, deck, round
+
+  beforeEach(() => {
+    card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    deck = new Deck([card1, card2, card3])
+    round = new Round(deck)
+  })
   
   it('should be a function', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
     expect(Round).to.be.a('function')
   })
 
   it('should be an instance of a round', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
     expect(round).to.be.an.instanceof(Round)
   })
 
   it('should have current card be first card in deck', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
     expect(round.currentCard).to.equal(card1)
   })
 
   it('should return current card in the deck', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     expect(round.returnCurrentCard()).to.equal(deck.deck[0])
   })
 
   it('should store current turn count', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
     expect(round.turns).to.equal(0)
   })
   
   it('should update current turn count', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     round.takeTurn('dog')
-
     expect(round.turns).to.equal(1)
   })
 
   it('should make the next card the current card', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     round.takeTurn()
-
     expect(round.currentCard).to.equal(card2)
   })
 
   it('should store incorrect guesses', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     round.takeTurn('pug')
-
     expect(round.incorrectGuesses).to.deep.equal([1])
   })
   
   it('should return Correct! when the answer is correct', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     expect(round.takeTurn('sea otter')).to.equal('Correct!')
   })
 
   it('should return Incorrect! when the answer is incorrect', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     expect(round.takeTurn('pug')).to.equal('Incorrect!')
   })
 
   it('should calculate percent of questions that were guessed correctly', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
     round.takeTurn('wrong')
     round.takeTurn('gallbladder')
     round.takeTurn('wrong')
     round.calculatePercentCorrect() 
-
     expect(round.calculatePercentCorrect()).to.equal(33)
   })
 
   it.skip('should print  ** Round over! ** You answered 33% of the questions correctly! to the console', () => {
-    const card1 = new Card (1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card (2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(3, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3])
-   // const turn = new Turn('gallbladder', card2)
-    const round = new Round(deck)
-
     round.takeTurn('wrong')
     round.takeTurn('gallbladder')
     round.takeTurn('wrong')
     round.calculatePercentCorrect() 
-
     expect(round.endRound()).to.equal('** Round over! ** You answered 33% of the questions correctly!')
   })
-
-
-
 })
